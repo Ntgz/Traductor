5e7e89ff81358ac7905bda6897871a981858bcb2
Ntgz <nicotellezg@hotmail.com>
2024-02-15 22:19:32 -0500

    [add] logica

--- traductor/src/App.tsx
+++ traductor/src/App.tsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useReducer } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import { useStore } from './hooks/useStore';
+import {AUTO_LANGUAGE} from './constants'
+
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+   const { fromLanguage, toLanguage, interChangeLanguages } = useStore()
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+
+    <Container fluid>
+      <h1>Google translate</h1>
+
+      <Row>
+        <Col>
+        <h2>From</h2>
+        {fromLanguage}
+        </Col>
+        <Col>
+        <button disabled = {fromLanguage === AUTO_LANGUAGE} onClick={
+          interChangeLanguages
+        }>
+          intercambiar
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        </Col>
+        <Col>
+        <h2>To</h2>
+        {toLanguage}
+        </Col>
+      </Row>
+
+      
+    </Container>
+    
   )
 }
 
